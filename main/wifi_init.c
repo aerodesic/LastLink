29db27f2e10cbc1e7b8af7b1f47ae4a64368a714
Gary Oliver <go@ao-cs.com>
2020-03-18 00:41:37 -0700

    Forgot to track the 'include' directory'

--- main/wifi_init.c
+++ main/wifi_init.c
@@ -2,13 +2,14 @@
  * wifi_init.c
  */
 #include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+//#include "freertos/FreeRTOS.h"
+//#include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "os_freertos.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
@@ -17,13 +18,13 @@
 
 static const char *TAG = "Wifi";
 
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
-                                    int32_t event_id, void* event_data)
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
                  MAC2STR(event->mac), event->aid);
+
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
         wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
@@ -61,7 +62,6 @@ void wifi_init_softap(void)
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s",
-             CONFIG_LASTLINK_WIFI_SSID, CONFIG_LASTLINK_WIFI_PASSWORD);
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s", CONFIG_LASTLINK_WIFI_SSID, CONFIG_LASTLINK_WIFI_PASSWORD);
 }
 
